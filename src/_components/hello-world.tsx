6bec9c2aed0fc6ea61ad820f5d4f6da44eb2aa4d
Nicha <NichaRoj@gmail.com>
2023-11-26 19:46:45 +0700

    run prettier

--- src/_components/hello-world.tsx
+++ src/_components/hello-world.tsx
@@ -8,7 +8,7 @@ export default function HelloWorld() {
   const [name, setName] = useState('')
   const [isLoading, setLoading] = useState(true)
 
-  function handleNameChange(e: any)  {
+  function handleNameChange(e: any) {
     setName(e.target.value)
   }
 
@@ -18,7 +18,7 @@ export default function HelloWorld() {
       method: 'POST',
       body: JSON.stringify({
         Name: name,
-      })
+      }),
     })
       .then((res) => res.text())
       .then((data) => {
@@ -26,7 +26,7 @@ export default function HelloWorld() {
         setLoading(false)
       })
       .catch((err) => {
-        setMessage("Error: " + err)
+        setMessage('Error: ' + err)
         setLoading(false)
       })
   }
@@ -35,8 +35,7 @@ export default function HelloWorld() {
     fetch(Config.EndPoint + '/hello-world', {
       mode: 'cors',
       method: 'POST',
-      body: JSON.stringify({
-      })
+      body: JSON.stringify({}),
     })
       .then((res) => res.text())
       .then((data) => {
@@ -44,7 +43,7 @@ export default function HelloWorld() {
         setLoading(false)
       })
       .catch((err) => {
-        setMessage("Error: " + err)
+        setMessage('Error: ' + err)
         setLoading(false)
       })
   }, [])
@@ -59,14 +58,18 @@ export default function HelloWorld() {
   return (
     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex text-center">
       <div>{message}</div>
-      <div><input
-      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-      type="text"
-      value={name}
-      onChange={handleNameChange}
-      placeholder='Name'
-      /></div>
-      <div><button onClick={handleSayHiClick}>Say Hi</button></div>
+      <div>
+        <input
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          placeholder="Name"
+        />
+      </div>
+      <div>
+        <button onClick={handleSayHiClick}>Say Hi</button>
+      </div>
     </div>
   )
 }
